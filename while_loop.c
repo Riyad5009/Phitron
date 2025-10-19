cd52ff400c5a21247b05b6c0725c5006ec07b581
Abrar <rmd394195@gmial.comn>
2025-10-19 16:33:25 +0600

    while

---
+++ while_loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i;
+          //condition
+    while(i<=10){
+        printf("%d\n",i);
+        //increment decriment
+        i=i+1;
+    }
+    return 0;
+}
