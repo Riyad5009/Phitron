d833309f14e2f4b84a348187a09ca4f832c728ca
Abrar <rmd394195@gmial.comn>
2025-12-17 16:04:30 +0600

    lexioco

---
+++ lexicograph.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    char a[100],b[100];
+    scanf("%s%s",&a,&b);
+    int i=0;
+    while(1){
+        if(a[i]=='\0'&& b[i]=='\0')
+        {
+            printf("Same\n");
+            break;
+        }
+      else if(a[i]=='\0')
+      {
+        printf("a choto");
+        break;
+      }
+      else if(b[i]=='\0')
+      {
+        printf("B choto");
+        break;
+      }
+      if(a[i]==b[i])
+      {
+        i++;
+      }
+      else if(a[i]<b[i])
+      {
+        printf("A choto\n");
+        break;
+      }
+      else{
+        printf("b coto");
+        break;
+      }
+
+    }
+   return 0;
+}
